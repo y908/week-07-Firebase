d4f0b62517dc58f6374f356aa8f1682696fb7d95
Yana Yakubchik <Yana Yakubchik>
2016-06-20 15:10:39 -0400

    I added full javascript file

---
+++ assets/js/train.js
@@ -0,0 +1,81 @@
+$( document ).ready(function() {
+
+var train = '';
+var destination = '';
+var firstTrain = '';
+var frequency = '';
+
+//Firebase URL 
+var dataRef = new Firebase("https://train101.firebaseio.com/");
+
+  //gives input a value
+  $('#addTrainBtn').on('click', function() {
+    train = $('#trainNameInput').val();
+    destination = $('#destinationInput').val();
+    firstTrain = $('#firstTrainInput').val();
+    frequency = $('#frequencyInput').val();
+
+    //empty after addTrain button is clicked
+    $('#trainNameInput').val('');
+    $('#destinationInput').val('');
+    $('#firstTrainInput').val('');
+    $('#frequencyInput').val('');
+
+    //put this to firebase
+    dataRef.push({
+
+      train: train,
+      destination: destination,
+      firstTrain: firstTrain,
+      frequency: frequency
+
+    });
+
+    return false;
+
+
+  });// addBtn click ending
+
+    // Child function
+
+    dataRef.on("child_added", function(snapshot) {
+
+    // Logs user-input 
+    console.log(snapshot.val().train + " = train");
+    console.log(snapshot.val().destination + " = destination");
+    console.log(snapshot.val().firstTrain + " = nextTrain");
+    console.log(snapshot.val().frequency +" = frequency");
+
+    // Variables assigned to equal value of child_added inputs
+    var train = snapshot.val().train;
+    var destination = snapshot.val().destination;
+    var firstTrain = snapshot.val().firstTrain;
+    var frequency = snapshot.val().frequency;
+
+    // Moment JS
+    var timeHour = moment().format('H');
+    var timeMin = moment().format('m');
+    var ftHour = moment(firstTrain, "HH:mm").format('H');
+    var ftMin = moment(firstTrain, "HH:mm").format('m');
+
+    var ftMoment = (ftHour * 60) + (ftMin * 1);
+    var timeMoment = (timeHour * 60) + (timeMin * 1);
+
+  //time passed since the first train
+    var diff = timeMoment - ftMoment;
+
+  //
+    var trainsSinceFirst = Math.floor(diff/frequency);
+
+  // Find how long until the next train comes
+    var nextArrival = ((trainsSinceFirst + 1) * frequency) + ftMoment;
+    
+
+  // Appends new info to table
+  $("#trainData").append("<tr><td>" + train + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minAway + "</td></tr>");
+
+    
+
+    }); 
+
+});
